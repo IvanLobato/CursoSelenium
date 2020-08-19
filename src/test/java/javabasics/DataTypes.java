6e92dd16d2c06654ae43891d78af22d577eec9e4
IvanLobato <69393969+IvanLobato@users.noreply.github.com>
2020-08-18 20:19:45 -0500

    Tercer Commit

--- src/test/java/javabasics/DataTypes.java
+++ src/test/java/javabasics/DataTypes.java
@@ -4,19 +4,51 @@ public class DataTypes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		byte x;
-        short y;
-        int z;
-        long a;
+	/*
+	 *Datos numericos
+	 */	
+		
+	//	byte x;
+    //    short y;
+    //    int z;
+    //    long a;
        
-        x=127;
-        y=32767;
-        z=2147483647;
-        a=9223372036854775807L;
-       
-        System.out.println(x);
+    //  y=32767;
+    //  z=2147483647;
+    //  a=9223372036854775807L;
+        
+    //  System.out.println(a);
+      
        // Colocan syso y seguido Ctrl + la tecla de espacio y se los autocompleta!!
 	
+	/*
+	 *Datos numericos reales
+	 */	
+		
+	//float base;
+	//double altura;
+		
+	//base = 30.5f;
+	//altura = 40.76;
+	
+	//System.out.println(base);
+	
+	//char letra1;
+	//char letra2;
+	//String frase;
+	
+	//letra1 = 'a';
+	//letra2 = '\u0047';
+	
+	//frase = "Hola";
+	//System.out.println(frase);
+		
+	boolean avesvuelan;
+	
+	avesvuelan = true;
+	
+	System.out.println("Las aves vuelan?" + avesvuelan);
+		
 	}
 
 }
--- src/test/java/javabasics/OperatorsStatements.java
+++ src/test/java/javabasics/OperatorsStatements.java
@@ -5,9 +5,43 @@ public class OperatorsStatements {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	/*		
+	 *Operadores Aritmeticos
+	 */
+	//int x = 1;
+	//int y = 2;
+	//int suma;
+	
+	//suma = x+y;
+	//System.out.println(suma);
+		
+	/*
+	 * Operadores unarios
+	 */
+	
+	//	int valor1 = 3;
+	//	int incrementar = --valor1;
+		
+	//	System.out.println(incrementar);
+	
+	/*
+	 * Operadores Relacionales	
+	 */
+		
+		int num1 = 8;
+		int num2 = 8;
+		int num3 = 6;
+		int num4 = 11;
+				
+		if (num1 >= 10 && num2 > 10) {
+			System.out.println("Los numeros son mayores a 10");
+		}else {
+			System.out.println("Uno de los numeros o Los dos numeros son menores a 10");
+		}	
+		
+	 }
+		
 			
-		
-		
 	}
 
-}
+
