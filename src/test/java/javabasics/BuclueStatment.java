0e3dd6d79aed03adafae91d84351b54e329745fa
IvanLobato <69393969+IvanLobato@users.noreply.github.com>
2020-08-25 16:01:26 -0500

    Five

---
+++ src/test/java/javabasics/BuclueStatment.java
@@ -0,0 +1,38 @@
+package javabasics;
+
+public class BuclueStatment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//	int numero = 1;
+//	
+//	while(numero<10) {
+//		System.out.println("el numero es" + numero);
+//		++ numero;
+//	    System.out.println("valor actual numero es:" + numero);
+
+	    //DO WHILE
+//		
+//	int numero = 10;
+//	int count = 0;
+//	int suma = 0;
+//	
+//	do {
+//		suma = suma + (numero+numero);
+//		System.out.println("La suma hasta el momento es:"+suma);
+//		++count;
+//		
+//	} while (suma<20);
+//       System.out.println("El numero se sumo:"+ count);
+//      
+		//FOR
+	int numero = 1;
+	
+	for (int i = 0; i <= numero; i++) {
+		System.out.println("numero:" + i);
+	}
+	}
+
+	}
+
